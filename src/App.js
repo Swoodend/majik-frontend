32c6e9256c4336fc11e1cb293ee92374ff82451b
Scott Woodend <woodendscott@gmail.com>
2017-11-10 16:35:26 -0500

    Includes a reset button for the user to bring all of their SubComponents back with height and width set to 100%, some default text.
    Reset function will reset the SubComponents by sending them new props. This likely is not the best way to do this and can be improved in the future.

--- src/App.js
+++ src/App.js
@@ -10,10 +10,13 @@ class App extends Component {
   constructor(props){
     super(props);
     this.handleClick = this.handleClick.bind(this);
-    
+    this.reset = this.reset.bind(this);
+
     this.state = {
       orientation: 'horizontal',
-      hidden: []
+      defaultHeight: 100,
+      defaultText: "SubComponent",
+      reset: false
     };
   }
 
@@ -23,13 +26,59 @@ class App extends Component {
     });
   }
 
+  reset(){
+    //this is stupid but it works for now
+    this.setState({
+      reset: {
+        text: "SubComponent"
+      }
+    }, () => {
+      this.setState({
+        reset: false
+      })
+    })
+  }
+
   render() {
-    let subContainers = [<SubContainer orientation={this.state.orientation} index={0} key={0} bc="deepskyblue">Hello</SubContainer>, <SubContainer orientation={this.state.orientation} index={1} key={1} bc="salmon">World</SubContainer>]
     return (
       <div className="App">
         <h1>Click on a subcontainer to hide it</h1>
-        <button onClick={this.handleClick}>Switch orientation</button>        
-        <Container subContainers={subContainers} orientation={this.state.orientation} hidden={this.state.hidden}/>
+        <button onClick={this.handleClick}>Switch orientation</button>
+        <button onClick={this.reset}>Reset</button>     
+        <Container orientation={this.state.orientation} hidden={this.state.hidden}>
+          <SubContainer 
+            reset={this.state.reset} 
+            height={this.state.defaultHeight} 
+            width={this.state.defaultHeight} 
+            orientation={this.state.orientation} 
+            index={0} 
+            key={0} 
+            bc="lightpink"
+          >
+            {this.state.defaultText}
+          </SubContainer>
+          <SubContainer 
+            reset={this.state.reset} 
+            height={this.state.defaultHeight} 
+            width={this.state.defaultHeight} 
+            orientation={this.state.orientation} 
+            index={1} 
+            key={1} 
+            bc="lightyellow"
+            >
+              {this.state.defaultText}
+          </SubContainer>
+          <SubContainer 
+            reset={this.state.reset} 
+            height={this.state.defaultHeight} 
+            width={this.state.defaultHeight} 
+            orientation={this.state.orientation} 
+            index={2} 
+            key={2} 
+            bc="#7B3F00">
+            {this.state.defaultText}
+          </SubContainer>
+        </Container>
       </div>
     );
   }
