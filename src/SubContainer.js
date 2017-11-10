7e2158f46352434405338289c0f51bde400f1a7b
Scott Woodend <woodendscott@gmail.com>
2017-11-10 15:07:13 -0500

    removes the text of the SubComponent when clicked upon. Previously the text would persist after the component itself was set to width 0

--- src/SubContainer.js
+++ src/SubContainer.js
@@ -7,13 +7,15 @@ export default class SubContainer extends Component{
         this.handleClick = this.handleClick.bind(this);
         this.state = {
             width: 100,
-            height: 100
+            height: 100,
+            text: this.props.children
         }
     }
 
     handleClick(){
         this.setState({
-            width: 0
+            width: 0,
+            text: null
         })
     }
 
@@ -26,6 +28,7 @@ export default class SubContainer extends Component{
                     <div onClick={this.handleClick}
                         style={{width: `${value.width}%`, height:"100%", backgroundColor:this.props.bc}}
                     >
+                        {this.state.text}
                     </div>
                 }
             </Motion>
