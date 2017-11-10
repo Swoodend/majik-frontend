1169917c69c4ce4241cf6ac8cef64d7b77a6bed6
Scott Woodend <woodendscott@gmail.com>
2017-11-10 15:32:45 -0500

    removes all the logic and elements associated with hiding a SubContainer as they are now hidden using react-motion

--- src/App.js
+++ src/App.js
@@ -10,8 +10,6 @@ class App extends Component {
   constructor(props){
     super(props);
     this.handleClick = this.handleClick.bind(this);
-    this.addHiddenIndex = this.addHiddenIndex.bind(this);
-    this.revealAllSubContainers = this.revealAllSubContainers.bind(this);
     
     this.state = {
       orientation: 'horizontal',
@@ -25,29 +23,12 @@ class App extends Component {
     });
   }
 
-  addHiddenIndex(index){
-    let {hidden} = this.state;
-    
-    hidden.push(index);
-    
-    this.setState({
-      hidden
-    });
-  }
-
-  revealAllSubContainers(){
-    this.setState({
-      hidden: []
-    });
-  }
-
   render() {
     let subContainers = [<SubContainer orientation={this.state.orientation} index={0} key={0} bc="deepskyblue">Hello</SubContainer>, <SubContainer orientation={this.state.orientation} index={1} key={1} bc="salmon">World</SubContainer>]
     return (
       <div className="App">
-        <button onClick={this.handleClick}>Switch orientation</button>
-        <button onClick={this.revealAllSubContainers}>Unhide all containers</button>
-        <HideContainerButtons subContainers={subContainers} addToHidden={this.addHiddenIndex}/>
+        <h1>Click on a subcontainer to hide it</h1>
+        <button onClick={this.handleClick}>Switch orientation</button>        
         <Container subContainers={subContainers} orientation={this.state.orientation} hidden={this.state.hidden}/>
       </div>
     );
