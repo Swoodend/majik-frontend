cde1887d37d46803089ab0dda0340233b7caa826
Scott Woodend <woodendscott@gmail.com>
2017-11-10 14:39:57 -0500

    wraps SubContainers in a <Motion/> component which will set either height or width (depending on orientation) to 0 to hide the SubContainer when it is clicked upon

--- src/SubContainer.js
+++ src/SubContainer.js
@@ -1,29 +1,34 @@
 import React, { Component } from 'react';
+import { Motion, spring } from 'react-motion';
 
 export default class SubContainer extends Component{
     constructor(props){
         super(props)
+        this.handleClick = this.handleClick.bind(this);
         this.state = {
             width: 100,
             height: 100
         }
     }
 
-    render(){
-        let { bc } = this.props;
-        let height = `${this.state.height}%`
-        let width = `${this.state.width}%`
+    handleClick(){
+        this.setState({
+            width: 0
+        })
+    }
 
-        let subContainerStyle = {
-            backgroundColor: bc,
-            height,
-            width
-        }
-        
+    render(){
         return (
-            <div style={subContainerStyle}>
-                {this.props.children}
-            </div>
+            <Motion 
+                originalStyle={{width: 100}} 
+                style={{width: spring(this.state.width, {stiffness: 50, damping: 5})}}>
+                {value => 
+                    <div onClick={this.handleClick}
+                        style={{width: `${value.width}%`, height:"100%", backgroundColor:this.props.bc}}
+                    >
+                    </div>
+                }
+            </Motion>
         )
     }
 }
