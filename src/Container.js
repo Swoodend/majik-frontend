a83613b50aae27f34f8eb312ff5e50290c9ca559
Scott Woodend <woodendscott@gmail.com>
2017-11-10 13:31:56 -0500

    Removes styling and logic of hiding a SubComponent. This will now be handled with react-motion adjusting the width of the SubContainer components

--- src/Container.js
+++ src/Container.js
@@ -2,44 +2,10 @@ import React , { Component } from 'react';
 
 export default class Container extends Component {
     
-    
-    
-    render(){
-        let subContainerStyle= {
-            width: "100%",
-            height: "100%",
-            backgroundColor: "red"
-        };
-
-
-        let subContainers = this.props.subContainers.map((subContainer, i) => {
-
-            if (this.props.hidden.indexOf(i) > -1){
-                return (
-                    <div
-                        key={i}
-                        style={subContainerStyle}
-                        className={"sub-container hidden"}
-                    >
-                        {subContainer}
-                    </div>
-                )
-            } else {
-                return (
-                    <div 
-                        key={i} 
-                        style={subContainerStyle} 
-                        className={"sub-container"}>
-                        {subContainer}
-                    </div>
-                );
-            }
-        
-
-        });
+    render(){        
         return (
             <div style={{width:"100vw", height:"100vh"}} className={"container" + " " + this.props.orientation}>
-                {subContainers}
+                {this.props.subContainers}
             </div>
         );
     }
