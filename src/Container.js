83ca22f1d4cdb7c7e17a725252534c1182907c13
Scott Woodend <woodendscott@gmail.com>
2017-11-10 15:26:54 -0500

    spacing

--- src/Container.js
+++ src/Container.js
@@ -1,8 +1,8 @@
 import React , { Component } from 'react';
 
 export default class Container extends Component {
-    
-    render(){        
+
+    render(){
         return (
             <div style={{width:"100vw", height:"100vh"}} className={"container" + " " + this.props.orientation}>
                 {this.props.subContainers}
